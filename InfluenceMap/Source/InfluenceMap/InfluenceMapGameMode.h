5e46523e932d25e665c8823401543e4438d91ef2
Charles-Antoine BOULANGER <charlesantoine.boulanger@gmail.com>
2023-06-11 18:27:37 +0200

    UE project

---
+++ InfluenceMap/Source/InfluenceMap/InfluenceMapGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfluenceMapGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInfluenceMapGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInfluenceMapGameMode();
+};
+
+
+
